36e6f74a50ace14a0bb878c95f6940835e230887
corey_hughes <vertical.consulting17@gmail.com>
2024-03-09 15:48:03 -0600

    removed secret

--- gpt_summary.py
+++ gpt_summary.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = ""
 
 def get_summary_for_resume(description):
     response = openai.Completion.create(
